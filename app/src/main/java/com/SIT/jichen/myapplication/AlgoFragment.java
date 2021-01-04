7bc25f2b1892fb28c2316549023d839a38a0df76
DaiJichen <1245487099@qq.com>
2021-01-04 00:04:14 -0500

    change relative place for algorithm layout

--- app/src/main/java/com/SIT/jichen/myapplication/AlgoFragment.java
+++ app/src/main/java/com/SIT/jichen/myapplication/AlgoFragment.java
@@ -2,6 +2,8 @@ package com.SIT.jichen.myapplication;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,17 +17,8 @@ import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AlgoFragment extends Fragment {
-    FloatingActionButton fab;
+    FloatingActionButton floatingActionButton;
     AppBarLayout appBarLayout;
-
-    LogFragment logFragment;
-    CodeFragment codeFragment;
-    AlgoDescriptionFragment algoFragment;
-    ViewPager viewPager;
-
-    Algorithm algorithm;
-
-    String startCommand = Algorithm.COMMAND_START_ALGORITHM;
 
     public static AlgoFragment getInstance(String algoName) {
         AlgoFragment algoFragment = new AlgoFragment();
@@ -40,17 +33,8 @@ public class AlgoFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_algo, container, false);
 
-        appBarLayout = (AppBarLayout) rootView.findViewById(R.id.app_bar);
-
-
-        fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-        viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
-
-        logFragment = LogFragment.newInstance();
-        codeFragment = CodeFragment.newInstance(getArguments().getString(Algorithm.KEY_ALGORITHM));
-        algoFragment = AlgoDescriptionFragment.newInstance(getArguments().getString(Algorithm.KEY_ALGORITHM));
-
-        setupFragment(getArguments().getString(Constants.ALGORITHM_KEY));
+        appBarLayout = (AppBarLayout) rootView.findViewById(R.id.appBarLayout);
+        floatingActionButton = (FloatingActionButton) rootView.findViewById(R.id.floatingActionButton);
 
         return rootView;
     }
