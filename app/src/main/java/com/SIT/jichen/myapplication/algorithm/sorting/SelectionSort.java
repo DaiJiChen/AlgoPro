ff451cbd554a785bcf0c923186ee4dd3b68f5e19
DaiJichen <1245487099@qq.com>
2021-01-06 02:08:57 -0500

    Create SelectionSort.java

---
+++ app/src/main/java/com/SIT/jichen/myapplication/algorithm/sorting/SelectionSort.java
@@ -0,0 +1,57 @@
+package com.SIT.jichen.myapplication.algorithm.sorting;
+
+import android.app.Activity;
+
+import com.SIT.jichen.myapplication.Constants;
+import com.SIT.jichen.myapplication.visualization.SortingVisualizer;
+
+public class SelectionSort extends SortAlgorithm {
+
+    int[] array;
+
+    public SelectionSort(SortingVisualizer visualizer, Activity activity) {
+        this.visualizer = visualizer;
+        this.activity = activity;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    private void sort() {
+        logArray("Original array - " ,array);
+
+        int n = array.length;
+        for (int i = 0; i < n-1; i++) {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (array[j] < array[min_idx]) {
+                    min_idx = j;
+                }
+            int temp = array[min_idx];
+            array[min_idx] = array[i];
+            addLog("Swapping " + array[i] + " and " + temp);
+            highlightSwap(min_idx,i);
+            array[i] = temp;
+            sleep();
+        }
+        addLog("Array has been sorted");
+        completed();
+    }
+
+    @Override
+    public void onDataRecieved(Object data) {
+        super.onDataRecieved(data);
+        this.array = (int[]) data;
+    }
+
+    @Override
+    public void onMessageReceived(String message) {
+        super.onMessageReceived(message);
+        if (message.equals(Constants.COMMAND_START_ALGORITHM)) {
+            startExecution();
+            sort();
+        }
+    }
+}
